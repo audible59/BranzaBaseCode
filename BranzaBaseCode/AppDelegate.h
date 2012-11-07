2b15d73054a57cc52620ed2aa0e881d5b9ce90d1
Kevin Rafferty <audible59@yahoo.com>
2012-11-06 21:03:45 -0800

    Initial Commit

---
+++ BranzaBaseCode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BranzaBaseCode
+//
+//  Created by Kevin E. Rafferty II on 11/6/12.
+//  Copyright (c) 2012 Branza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BranzaBaseCode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BranzaBaseCode
+//
+//  Created by Kevin E. Rafferty II on 11/6/12.
+//  Copyright (c) 2012 Branza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
