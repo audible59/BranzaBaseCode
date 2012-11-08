739a9bf83e41c9a01aef537989d6ef484d111f1e
Kevin Rafferty <audible59@yahoo.com>
2012-11-07 22:13:03 -0800

    Added Social Media Framework fro Twitter and Facebook

--- BranzaBaseCode/LocationViewController.h
+++ BranzaBaseCode/LocationViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface LocationViewController : UIViewController
+@interface LocationViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>
+{
+    CLLocationManager *locationManager;
+}
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- BranzaBaseCode/LocationViewController.m
+++ BranzaBaseCode/LocationViewController.m
@@ -14,25 +14,64 @@
 
 @implementation LocationViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+@synthesize mapView = _mapView;
+
+#pragma mark -
+#pragma mark View Life Cycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    [self.navigationController.navigationBar.topItem setTitle:@"Location"];
+    
+    self.mapView.delegate = self;
+    
+    locationManager = [[CLLocationManager alloc] init];
+    [locationManager setDelegate:self];
+    
+    [locationManager setDistanceFilter:kCLDistanceFilterNone];
+    [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    
+    [self.mapView setShowsUserLocation:YES];
+        
+    CLLocationCoordinate2D zoomLocation;
+    
+    zoomLocation.latitude  = 33.675749;
+    zoomLocation.longitude = -117.886806;
+    
+    MKPointAnnotation *annotationPoint = [[MKPointAnnotation alloc] init];
+    annotationPoint.coordinate         = zoomLocation;
+    annotationPoint.title              = @"Branza";
+    annotationPoint.subtitle           = @"Branza's headquarters";
+    
+    [_mapView addAnnotation:annotationPoint];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark MapKit Delegate Methods
+
+- (void)mapView:(MKMapView *)mv didAddAnnotationViews:(NSArray *)views
+{
+    CLLocationCoordinate2D zoomLocation;
+    
+    zoomLocation.latitude  = 33.675749;
+    zoomLocation.longitude = -117.886806;
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5 * METERS_PER_MILE, 0.5 * METERS_PER_MILE);
+    
+    [mv setRegion:region animated:YES];
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    self.mapView.centerCoordinate = userLocation.location.coordinate;
 }
 
 @end
