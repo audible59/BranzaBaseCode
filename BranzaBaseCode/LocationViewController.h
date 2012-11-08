96c758d1a1ed6a3882541228951d9517b29828e6
Kevin Rafferty <audible59@yahoo.com>
2012-11-07 16:54:07 -0800

    Added Basic LocationViewController
    This controller simply loads in and zooms to a specified location using
    Apple's MapKit API.

---
+++ BranzaBaseCode/LocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationViewController.h
+//  BranzaBaseCode
+//
+//  Created by Kevin E. Rafferty II on 11/7/12.
+//  Copyright (c) 2012 Branza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationViewController : UIViewController
+
+@end
---
+++ BranzaBaseCode/LocationViewController.m
@@ -0,0 +1,38 @@
+//
+//  LocationViewController.m
+//  BranzaBaseCode
+//
+//  Created by Kevin E. Rafferty II on 11/7/12.
+//  Copyright (c) 2012 Branza. All rights reserved.
+//
+
+#import "LocationViewController.h"
+
+@interface LocationViewController ()
+
+@end
+
+@implementation LocationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
